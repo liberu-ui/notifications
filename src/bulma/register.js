67650658aaf3e926c60e2869603112f6eca4eebf
mishu <mishu@xtelecom.ro>
2022-01-17 11:27:35 +0200

    updates navbar registration

--- src/bulma/register.js
+++ src/bulma/register.js
@@ -2,5 +2,5 @@ import App from '@enso-ui/ui/src/core/app';
 import Notification from './components/navbar/Notifications.vue';
 import ToastPosition from './components/settings/ToastrPosition.vue';
 
-App.registerNavbarItem('navbar-notification', Notification, 300, 'core.notifications.count');
-App.registerSettingsItem('setting-toaster-position', ToastPosition, 300);
+App.registerNavbarItem('notification', Notification, 300, 'core.notifications.count');
+App.registerSettingsItem('toaster-position', ToastPosition, 300);
